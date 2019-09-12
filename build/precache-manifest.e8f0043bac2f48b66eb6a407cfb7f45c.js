c3e7353200a60b6fb9e48fc094e20c938a9fb2a3
ron litzenberger <ron.litzenberger@nearform>
2019-09-11 21:34:15 -0500

    initial commit

---
+++ build/precache-manifest.e8f0043bac2f48b66eb6a407cfb7f45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f82711c14af91735814141afe0ea47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed13f20a9c944c076aad",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "29af770064738b309189",
+    "url": "/static/js/2.27c5d92a.chunk.js"
+  },
+  {
+    "revision": "ed13f20a9c944c076aad",
+    "url": "/static/js/main.08dc8dfe.chunk.js"
+  },
+  {
+    "revision": "d4c130e6f8a4e3f2d029",
+    "url": "/static/js/runtime~main.e7f4665f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
