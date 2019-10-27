5060fb78e65c4095ffca413692d93b47d7d2c4d7
ron litzenberger <ron.litzenberger@gmail.com>
2019-10-26 19:10:27 -0500

    Updates

---
+++ precache-manifest.885ae84f0408bc65482dc922a9db59f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b704768b26afb83078ec7d166dd8d19",
+    "url": "/arcadequiz/index.html"
+  },
+  {
+    "revision": "8ab3d0c18e87510ea551",
+    "url": "/arcadequiz/static/css/2.84bfa44f.chunk.css"
+  },
+  {
+    "revision": "10494aae6f7859804220",
+    "url": "/arcadequiz/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8ab3d0c18e87510ea551",
+    "url": "/arcadequiz/static/js/2.e593ee9f.chunk.js"
+  },
+  {
+    "revision": "10494aae6f7859804220",
+    "url": "/arcadequiz/static/js/main.c440acc0.chunk.js"
+  },
+  {
+    "revision": "70ed3f61c67719d8efb1",
+    "url": "/arcadequiz/static/js/runtime~main.c5261426.js"
+  }
+]);
